0e718b0dfd70ed81d1f104e20466d7a7efe00666
al-pet <apetrov1024@gmail.com>
2023-05-08 21:54:18 +0300

    feat: fulfill task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const promises = require('fs/promises');
+const path = require('path');
 
+
+async function mkDir() {
+    try {
+
+        await promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function copyDir() {
+    try {
+
+        const copyFiles = await promises.readdir(path.join(__dirname, 'files-copy'));
+        for (let file of copyFiles) {
+            await promises.unlink(path.join(__dirname, 'files-copy', `${file}`),
+                { recursive: true, force: true });
+        }
+
+        const files = await promises.readdir(path.join(__dirname, 'files'));
+        for (let file of files) {
+            await promises.copyFile(path.join(__dirname, 'files', `${file}`), path.join(__dirname, 'files-copy', `${file}`));
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+mkDir();
+copyDir();
